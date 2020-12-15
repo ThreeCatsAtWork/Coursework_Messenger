4f711feb0f11511bd0a6ac3084413929a6a2c527
Alex Petrov <sasagromila@gmail.com>
2020-12-15 21:19:13 +0300

    Add MainForm

---
+++ DotChatWF/MainForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace DotChatWF
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.components = new System.ComponentModel.Container();
+      this.listMessages = new System.Windows.Forms.ListBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.fieldUsername = new System.Windows.Forms.TextBox();
+      this.fieldMessage = new System.Windows.Forms.TextBox();
+      this.btnSend = new System.Windows.Forms.Button();
+      this.updateLoop = new System.Windows.Forms.Timer(this.components);
+      this.btnAuth = new System.Windows.Forms.Button();
+      this.btnReg = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // listMessages
+      // 
+      this.listMessages.FormattingEnabled = true;
+      this.listMessages.Location = new System.Drawing.Point(9, 10);
+      this.listMessages.Margin = new System.Windows.Forms.Padding(2);
+      this.listMessages.Name = "listMessages";
+      this.listMessages.Size = new System.Drawing.Size(583, 277);
+      this.listMessages.TabIndex = 0;
+      this.listMessages.SelectedIndexChanged += new System.EventHandler(this.ListMessages_SelectedIndexChanged);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(9, 306);
+      this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(55, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Username";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(9, 334);
+      this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(50, 13);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "Message";
+      // 
+      // fieldUsername
+      // 
+      this.fieldUsername.AutoCompleteCustomSource.AddRange(new string[] {
+            "You are not logged in"});
+      this.fieldUsername.Location = new System.Drawing.Point(81, 304);
+      this.fieldUsername.Margin = new System.Windows.Forms.Padding(2);
+      this.fieldUsername.Name = "fieldUsername";
+      this.fieldUsername.ReadOnly = true;
+      this.fieldUsername.Size = new System.Drawing.Size(267, 20);
+      this.fieldUsername.TabIndex = 3;
+      this.fieldUsername.Text = "You are not logged in";
+      this.fieldUsername.TextChanged += new System.EventHandler(this.FieldUsername_TextChanged);
+      // 
+      // fieldMessage
+      // 
+      this.fieldMessage.Location = new System.Drawing.Point(81, 332);
+      this.fieldMessage.Margin = new System.Windows.Forms.Padding(2);
+      this.fieldMessage.Multiline = true;
+      this.fieldMessage.Name = "fieldMessage";
+      this.fieldMessage.Size = new System.Drawing.Size(391, 34);
+      this.fieldMessage.TabIndex = 4;
+      this.fieldMessage.TextChanged += new System.EventHandler(this.FieldMessage_TextChanged);
+      // 
+      // btnSend
+      // 
+      this.btnSend.Location = new System.Drawing.Point(483, 304);
+      this.btnSend.Margin = new System.Windows.Forms.Padding(2);
+      this.btnSend.Name = "btnSend";
+      this.btnSend.Size = new System.Drawing.Size(108, 63);
+      this.btnSend.TabIndex = 5;
+      this.btnSend.Text = "SEND";
+      this.btnSend.UseVisualStyleBackColor = true;
+      this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+      // 
+      // updateLoop
+      // 
+      this.updateLoop.Interval = 1000;
+      this.updateLoop.Tick += new System.EventHandler(this.UpdateLoop_Tick);
+      // 
+      // btnAuth
+      // 
+      this.btnAuth.Location = new System.Drawing.Point(12, 374);
+      this.btnAuth.Margin = new System.Windows.Forms.Padding(2);
+      this.btnAuth.Name = "btnAuth";
+      this.btnAuth.Size = new System.Drawing.Size(106, 23);
+      this.btnAuth.TabIndex = 6;
+      this.btnAuth.Text = "Authentification";
+      this.btnAuth.UseVisualStyleBackColor = true;
+      this.btnAuth.Click += new System.EventHandler(this.BtnAuth_Click);
+      // 
+      // btnReg
+      // 
+      this.btnReg.Location = new System.Drawing.Point(131, 374);
+      this.btnReg.Margin = new System.Windows.Forms.Padding(2);
+      this.btnReg.Name = "btnReg";
+      this.btnReg.Size = new System.Drawing.Size(106, 23);
+      this.btnReg.TabIndex = 7;
+      this.btnReg.Text = "Registartion";
+      this.btnReg.UseVisualStyleBackColor = true;
+      this.btnReg.Click += new System.EventHandler(this.BtnReg_Click);
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(601, 404);
+      this.Controls.Add(this.btnReg);
+      this.Controls.Add(this.btnAuth);
+      this.Controls.Add(this.btnSend);
+      this.Controls.Add(this.fieldMessage);
+      this.Controls.Add(this.fieldUsername);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.listMessages);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+      this.Margin = new System.Windows.Forms.Padding(2);
+      this.Name = "MainForm";
+      this.Text = "MainForm";
+      this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+      this.Load += new System.EventHandler(this.MainForm_Load);
+      this.VisibleChanged += new System.EventHandler(this.MainForm_VisibleChanged);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listMessages;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fieldUsername;
+        private System.Windows.Forms.TextBox fieldMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Timer updateLoop;
+    private System.Windows.Forms.Button btnAuth;
+    private System.Windows.Forms.Button btnReg;
+  }
+}
+
---
+++ DotChatWF/MainForm.cs
@@ -0,0 +1,243 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotChatWF
+{
+
+  public partial class MainForm : Form
+  {
+    public List<x_y> x_y = new List<x_y>();
+    // Глобальные переменные        
+    AuthentificationForm AuthForm;
+    RegistartionForm RegForm;
+    public TextBox TextBox_username;
+    public int int_token;       
+
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+
+    private void UpdateLoop_Tick(object sender, EventArgs e)
+    {      
+      List<Message> allMSG = GetAllMessages();                        
+      if (allMSG.Count > listMessages.Items.Count)
+      {
+        int index = -1;
+        listMessages.Items.Clear();
+        foreach(Message m in allMSG)
+        {
+          ++index;
+          listMessages.Items.Add($"{m.timestamp}  [{m.username}] {m.text}\tID: {index + 1}");
+        } 
+      }
+      else if (allMSG.Count < listMessages.Items.Count)
+      {
+        listMessages.Items.Clear();
+        int index = -1;
+        foreach(Message m in allMSG)
+        {
+          ++index;
+          listMessages.Items.Add($"{m.timestamp}  [{m.username}] {m.text}\tID: {index + 1}");
+        }
+      }
+    }
+
+    private void BtnSend_Click(object sender, EventArgs e)
+    {
+      if (int_token == 0)
+      {
+        MessageBox.Show("Please log in or register");
+      }
+      else
+      {
+        if(fieldUsername.Text == "admin")
+        {
+          if (fieldMessage.Text.IndexOf("/delete ") != -1 && fieldMessage.Text[0] == '/') // Если сообщения вида /delete ID, то удаляем сообщение с индексом ID
+          {
+            string[] request = fieldMessage.Text.Split(' ');
+            if (request.Length == 2)
+            {
+              var deleteRequest = (HttpWebRequest)WebRequest.Create("http://localhost:5000/api/Chat/" + $"{Convert.ToInt32(request[1])}"); // Реквест на api/Chat + ID
+              deleteRequest.Method = "DELETE";
+              deleteRequest.GetResponse();
+              fieldMessage.Text = String.Empty;
+            }
+            else // Если в окошке есть что-то помимо команды и ID
+            {
+              MessageBox.Show("Bad request");
+              fieldMessage.Text = String.Empty;
+            }
+          }
+          else // Если это обычное сообщение
+          {
+            SendMessage(new Message()
+            {
+              username = fieldUsername.Text,
+              text = fieldMessage.Text,
+            });
+            fieldMessage.Text = String.Empty;
+          }
+        }
+        else // Если это обычное сообщение
+        {
+          SendMessage(new Message()
+          {
+            username = fieldUsername.Text,
+            text = fieldMessage.Text,
+          });
+          fieldMessage.Text = String.Empty;
+        }
+      }
+    }
+
+    // Отправляет сообщение на сервер
+    void SendMessage(Message msg)
+    {
+      WebRequest req = WebRequest.Create("http://localhost:5000/api/chat");
+      req.Method = "POST";
+      string postData = JsonConvert.SerializeObject(msg);
+      //byte[] bytes = Encoding.UTF8.GetBytes(postData);
+      req.ContentType = "application/json";
+      //req.ContentLength = bytes.Length;
+      StreamWriter reqStream = new StreamWriter(req.GetRequestStream());
+      reqStream.Write(postData);
+      reqStream.Close();
+      req.GetResponse();
+    }
+
+    List<Message> GetAllMessages() // Спрашиваем список всех сообщений с сервера
+    {
+      var getRequest = (HttpWebRequest)WebRequest.Create("http://localhost:5000/api/chat");
+      using (StreamReader readList = new StreamReader(getRequest.GetResponse().GetResponseStream()))
+      {
+        return JsonConvert.DeserializeObject<List<Message>>(readList.ReadToEnd());
+      }
+    }
+
+    private void Get_x_y(object sender, EventArgs e)
+    {
+      string json = "";
+      using (StreamReader sr = new StreamReader("config.json", Encoding.Default))
+      {
+        json = sr.ReadToEnd();
+      }
+      var deserializer = new JsonSerializer();
+      x_y = (List<x_y>)deserializer.Deserialize(new StringReader(json), typeof(List<x_y>));
+
+      Size = new Size(x_y[0].x, x_y[0].y);
+    }
+
+    private void BtnAuth_Click(object sender, EventArgs e)
+    {
+      AuthForm = new AuthentificationForm
+      {
+        mForm = this
+      };
+      AuthForm.Show();
+      Visible = false;
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+      Get_x_y(sender, e);
+      int_token = 0;
+      AuthForm = new AuthentificationForm();
+      RegForm = new RegistartionForm();
+      TextBox_username = fieldUsername;
+    }
+
+    private void BtnReg_Click(object sender, EventArgs e)
+    {
+      RegForm = new RegistartionForm
+      {
+        mForm = this
+      };
+      RegForm.Show();
+      this.Visible = false;
+    }
+
+    private void ListMessages_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void MainForm_VisibleChanged(object sender, EventArgs e)
+    {
+      if (this.Visible == true && int_token != 0)
+      {
+        this.updateLoop.Enabled = true;
+      }
+    }
+
+    private void FieldMessage_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    // При закрытии формы отправляем то, что юзер вышел
+	  private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+	  {      
+      GettingOffline(TextBox_username.Text);
+	  }
+
+    // Метод, отправляющий серверу инфу, что мы вышли или разлогинились
+    public static void GettingOffline(string _login)
+    {
+      if (_login != "You are not logged in")
+		  {
+        var deleteRequest = WebRequest.Create("http://localhost:5000/api/Auth/" + $"{_login}");
+        deleteRequest.Method = "DELETE";
+        deleteRequest.GetResponse();
+      }      
+    }
+
+	  private void FieldUsername_TextChanged(object sender, EventArgs e)
+	  {
+
+	  }
+
+    private void listContacts_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+  }
+  public class x_y
+  {
+    public int x { get; set; }
+    public int y { get; set; }
+
+    public x_y()
+    {
+      x = 0;
+      y = 0;
+    }
+    public x_y(int x, int y)
+    {
+      this.y = x;
+      this.x = y;
+    }
+  }
+  [Serializable]
+  public class Message
+  {
+    public string username = "";
+    public string text = "";
+    public string timestamp;
+  }
+  public class Contacts
+  {
+    public string username = "";
+  }
+}
